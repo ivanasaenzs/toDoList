8d8a244e02027bdf62d1db53faf8adb298246c7a
ivanasaenzs <84483098+ivanasaenzs@users.noreply.github.com>
2024-06-18 15:47:12 -0300

    added material ui modal to confirm deletion of the selected task

---
+++ src/components/DeleteModal.jsx
@@ -0,0 +1,38 @@
+import * as React from "react";
+import { Button, Box, Typography, Modal } from "@mui/material";
+
+const style = {
+  position: "absolute",
+  top: "50%",
+  left: "50%",
+  transform: "translate(-50%, -50%)",
+  width: 400,
+  bgcolor: "background.paper",
+  border: "2px solid #000",
+  boxShadow: 24,
+  p: 4,
+};
+
+export default function BasicModal({ isOpen, handleClose, handleDelete }) {
+  return (
+    <div>
+      <Modal
+        open={isOpen}
+        onClose={handleClose}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+      >
+        <Box sx={style}>
+          <Typography id="modal-modal-title" variant="h6" component="h2">
+            Confirm Deletion
+          </Typography>
+          <Typography id="modal-modal-description" sx={{ mt: 2 }}>
+            Are you sure you want to delete this task?
+          </Typography>
+          <Button onClick={handleDelete}>Delete</Button>
+          <Button onClick={handleClose}>Cancel</Button>
+        </Box>
+      </Modal>
+    </div>
+  );
+}
