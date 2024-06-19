574229dcaae235a87d198a97be13d89ab8d7169c
ivanasaenzs <84483098+ivanasaenzs@users.noreply.github.com>
2024-06-19 16:28:20 -0300

    added EditTask component that handles the functionality and the cancelation of the action

---
+++ src/components/EditTask.jsx
@@ -0,0 +1,48 @@
+import React from "react";
+
+import { Button } from "@mui/material";
+import { updateLocalStorage } from "../utils/localStorage";
+
+export const EditTask = ({
+  id,
+  description,
+  tasks,
+  setTasks,
+  setTaskToEdit,
+}) => {
+  console.log(id);
+
+  // Cancel edit by setting the task to null
+  const handleCancelButton = () => {
+    setTaskToEdit(null);
+  };
+
+  // Handles the actual editing of the task
+  const handleEditTask = (e) => {
+    e.preventDefault();
+    console.log(e.target.taskDescription.value);
+
+    const editedArray = tasks.map((task) => {
+      if (task.id === id) {
+        return {
+          ...task,
+          description: e.target.taskDescription.value,
+        };
+      } else {
+        return task;
+      }
+    });
+    console.log("Updated Tasks Array: ", editedArray);
+    setTasks(editedArray);
+    updateLocalStorage(editedArray);
+    handleCancelButton();
+  };
+
+  return (
+    <form onSubmit={handleEditTask}>
+      <textarea defaultValue={description} name="taskDescription"></textarea>
+      <Button type="submit">Edit</Button>
+      <Button onClick={handleCancelButton}>Cancel</Button>
+    </form>
+  );
+};
