70e16e1667d002c87b1d92b2c26a5e692f5c0ef1
ivanasaenzs <84483098+ivanasaenzs@users.noreply.github.com>
2024-06-21 13:21:26 -0300

    form validations were added for the text input - onSubmit function that handles the addTask button was modified with react-hook-form's onSubmit function

--- src/components/AddTask.jsx
+++ src/components/AddTask.jsx
@@ -1,19 +1,26 @@
 import * as React from "react";
-import { useState } from "react";
+
 import { Box, Button, Container, TextField } from "@mui/material";
+import { useForm } from "react-hook-form";
 
 import SelectLabels from "./Select";
 
 import { IoAddCircle } from "react-icons/io5";
 
-export default function BasicTextFields({ addTask }) {
-  const [taskInInput, setTaskInInput] = useState("");
+export default function BasicTextFields({ addTask, tasks, setFilteredTasks }) {
+  const {
+    register,
+    formState: { errors },
+    handleSubmit,
+    reset,
+  } = useForm({
+    criteriaMode: "all",
+  });
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-
-    addTask(taskInInput);
-    setTaskInInput("");
+  // Handles the adding of a new task with react-hook-form's data element and resets the input field
+  const onSubmit = (data) => {
+    addTask(data.taskDescription);
+    reset({ taskDescription: "" });
   };
 
   return (
@@ -23,15 +30,13 @@ export default function BasicTextFields({ addTask }) {
         noValidate
         autoComplete="off"
         sx={{ display: "flex", alignItems: "center", gap: 1, width: "100%" }}
-        onSubmit={handleSubmit}
+        onSubmit={handleSubmit(onSubmit)}
       >
         <TextField
+          name="taskDescription"
+          placeholder="Enter your task"
           id="outlined-basic"
           variant="outlined"
-          value={taskInInput}
-          onChange={(e) => {
-            setTaskInInput(e.target.value);
-          }}
           sx={{
             backgroundColor: "#fff",
             width: "450px",
@@ -60,8 +65,19 @@ export default function BasicTextFields({ addTask }) {
               </Button>
             ),
           }}
+          error={!!errors.taskDescription}
+          helperText={
+            errors.taskDescription ? errors.taskDescription.message : ""
+          }
+          {...register("taskDescription", {
+            required: "You can't send an empty task",
+            maxLength: {
+              value: 50,
+              message: "The task is too long",
+            },
+          })}
         />
-        <SelectLabels />
+        <SelectLabels tasks={tasks} setFilteredTasks={setFilteredTasks} />
       </Box>
     </Container>
   );
